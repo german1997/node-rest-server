7cf5f2e11a82039a278edd5c2ec2f1d6a0b85f24
german pavez <ext_galbpavezm@falabella.cl>
2022-10-29 16:10:09 -0300

    add google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -52,6 +53,58 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await User.findOne({ correo });
+
+        if ( !usuario ) {
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new User( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en BD
+
+        if ( !usuario.status ){
+            return res.status(401).json({
+                msg: 'Hable con el adm, usuario bloqueado'
+            });
+        }
+
+        // Generar el jwt
+
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
